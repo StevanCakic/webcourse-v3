83fade6a8e00dd09fa0232c1e13d93206bddc735
StevanCakic <stevancakic93@gmail.com>
2019-10-21 10:53:11 +0200

    code for week 8 updated

--- Week 8/code-3/index.js
+++ Week 8/code-3/index.js
@@ -3,15 +3,10 @@
 // Prvo nam treba forma u kojoj unosimo ime itema
 // id - addForm
 
-const form = document.getElementById("addForm");
-
 // Treba nam container gdje se nalazi svi itemi
 // id - items
 
-const itemList = document.getElementById("items");
-
-// Na submit forme gdje unosimo item treba da aktiviramo submit event
-form.addEventListener("submit", addItem);
+// Na submit forme gdje unosimo item treba da aktiviramo submit event i zakacimo funkciju addItem
 
 // Sada trebamo da kreiramo event handler za submit forme
 // Event handler za dodavanje itema
@@ -19,53 +14,42 @@ function addItem(event) {
 
     // Na submit forme trebamo da onemogucimo default ponasanje forme
     // tj. da onemogucimo action na submit
-    event.preventDefault();
 
     // Sada nam treba vrijednost iz input polja
     // koju smo unijeli
     // id - item
-    const input = document.getElementById("item").value;
+
     // console.log(input);
 
     // Sledeci korak je kreiranje novog li itema
-    const li = document.createElement("li");
 
     // Nakon toga li itemu trebamo dodati klasu 
     // class - list-group-item
-    li.className = "list-group-item";
 
     // Nakon ovog trebamo da kreiramo text node
     // cija je vrijednost sacuvana u input polju
     // i da dodamo taj text node u li
 
-    li.appendChild(document.createTextNode(input));
-
     // Potrebno je da kreiramo i delete button element
-    const deleteBtn = document.createElement("button");
 
     // Potrebno je da dodamo klase za delete button
     // da bismo dobili adekvatan izgled
     // class - btn btn-danger btn-sm float-right delete
 
-    deleteBtn.className = "btn btn-danger btn-sm float-right delete";
-
     // Potrebno je da dodamo na delete button text node "X"
 
-    deleteBtn.appendChild(document.createTextNode("X"));
 
     // Sada moramo da dodamo deleteBtn u li
-    li.appendChild(deleteBtn);
 
     // Nakon toga, potrebno je da dodamo
     // novokreirani li u listu itema
-    itemList.appendChild(li);
+
 }
 
 // 2. Brisanje elemenata iz liste
 
-// Na item list dodamo click event i event handler za brisanje itema
+// Na item list dodamo click event i event handler za brisanje itema removeItem
 // U prvom koraku smo nasli item list !
-itemList.addEventListener("click", removeItem);
 
 // Sada trebamo da kreiramo event handler za brisanje itema
 function removeItem(event) {
@@ -73,7 +57,7 @@ function removeItem(event) {
     // sad treba da provjerimo da li je user kliknuo na X
     // to mozemo da odradimo tako sto provjerimo da li
     // element koji smo kliknuli sadrzi klasu "delete"
-    if (event.target.classList.contains("delete")) {
+    if (true/* change this */) {
         // ako sadrzi pozvati confirm sa porukom "Jeste li sigurni"
         // - radimo detaljnije nakon testa
         // confirm vrace true/false
@@ -82,12 +66,12 @@ function removeItem(event) {
             // prvo selektujemo li
             // li je parent node elementa X, tj. targeta
             // parentElement
-            const li = event.target.parentNode;
+            
 
             // onda obrisemo selektovani li
             // iz parent noda, tj. lista itema (definisana globalno)
             // parent.removeChild(child)
-            itemList.removeChild(li);
+
         }
     }
 }
@@ -96,35 +80,30 @@ function removeItem(event) {
 
 // prvo nam treba input polje za pretragu itema
 // id - filter
-const filter = document.getElementById("filter");
 
-// Na filter input polje dodamo event "keyup" i event handler
-filter.addEventListener("keyup", filterItems);
+
+// Na filter input polje dodamo event "keyup" i event handler filterItems
 
 // Event handler za filtriranje itema
 function filterItems(event) {
     // konvertovanje slova iz e.target.value u lowercase,
     // taj tekst cuvamo u varijablu 
     // lakse nam je da radimo pretragu
-    const text = event.target.value.toLowerCase();
 
     // uzmemo sve li iteme iz liste itema, gore vec definisali
     // sve elemente moramo da sacuvamo u varijablu
     // mozemo da koristimo getElementsByTagName
-    const liElements = itemList.getElementsByTagName("li");
-    console.log(liElements);
+
 
     // Konvertujemo items HTMLCollection u Array
     const liArray = Array.from(liElements);
-    console.log(liArray);
-
 
     // prolazimo kroz sve elemente niza
 
     liArray.forEach(function (item) {
         // iz svakog itema izvucemo text content
         // item.firstChild.textContent
-        const itemText = item.firstChild.textContent;
+        
         // provjerim da li se uneseni tekst nalazi u item text
         // Napomena: i item names moraju biti mala slova
         // Najjednostavnije koristiti indexOf za provjeru da li 
@@ -132,13 +111,9 @@ function filterItems(event) {
 
         if (itemText.toLowerCase().indexOf(text) != -1) {
             // ako jeste, item.style.display = "block"
-            item.style.display = "block";
+            
         } else {
             // ako nije, item.style.display = "none"
-            item.style.display = "none";
-
         }
-
     })
-
 }
--- Week 8/code-3/index_final.js
+++ Week 8/code-3/index_final.js
@@ -1,44 +1,31 @@
 var form = document.getElementById("addForm");
 var itemList = document.getElementById("items");
 var filter = document.getElementById("filter");
-
 // Form submit event
 form.addEventListener("submit", addItem);
-
 // Delete event
 itemList.addEventListener("click", removeItem);
-
 // Filter event
 filter.addEventListener("keyup", filterItems);
-
 // Add item
 function addItem(e) {
   e.preventDefault();
-
   //Get input value
   var newItem = document.getElementById("item").value;
-
   // Create new li element
   var li = document.createElement("li");
-
   // Add class
   li.className = "list-group-item";
-
   // Add text node with input value
   li.appendChild(document.createTextNode(newItem));
-
   // Create del button element
   var deleteBtn = document.createElement("button");
-
   // Add classes to del button
   deleteBtn.className = "btn btn-danger btn-sm float-right delete";
-
   // Append text node
   deleteBtn.appendChild(document.createTextNode("X"));
-
   // Append button to li
   li.appendChild(deleteBtn);
-
   // Append li to list
   itemList.appendChild(li);
 }
